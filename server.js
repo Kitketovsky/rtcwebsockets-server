d5277080b7f1fa7f68e27dbc7dc3ac6c210bbae6
Kitketovsky <kitketovsky@gmail.com>
2023-02-02 01:10:50 +0400

    Transition to Socket.IO

--- server.js
+++ server.js
@@ -1,52 +1,48 @@
-const WS = require('ws');
-const express = require('express');
+const { Server } = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = './index.html';
+const PORT = 8000;
 
-const app = express();
+const ACTIONS = {
+    CONNECTED: "connected",
+    JOIN: 'join',
+    NEW_CLIENT: "new_client",
+    OFFER: "offer",
+    ANSWER: "answer",
+    ICE: "ice",
+};
 
-app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-
-const httpServer = app.listen(PORT, () => console.log(`Server is listening on ${PORT}...`));
-const wss = new WS.WebSocketServer({ server: httpServer, host: process.env.HOST || 'localhost' });
-
-wss.on('connection', function connection(ws) {
-    ws.send(JSON.stringify({ type: "notification", msg: 'FROM SERVER:: You are connected to the server...' }));
-
-    ws.on('message', function message(data) {
-        const body = JSON.parse(data);
-
-        if (body.type === 'joined') {
-            wss.clients.forEach((client) => {
-                if (client !== ws && client.readyState === WS.WebSocket.OPEN) {
-                    client.send(JSON.stringify({ type: 'joined', msg: `A new user ${body.userId} joined the channel...`, id: body.userId }));
-                }
-            })
-        }
-
-        if (body.type === 'offer') {
-            wss.clients.forEach((client) => {
-                if (client !== ws && client.readyState === WS.WebSocket.OPEN) {
-                    client.send(JSON.stringify({ type: 'offer', offer: body.offer }));
-                }
-            })
-        }
-
-        if (body.type === 'answer') {
-            wss.clients.forEach((client) => {
-                if (client !== ws && client.readyState === WS.WebSocket.OPEN) {
-                    client.send(JSON.stringify({ type: 'answer', answer: body.answer }));
-                }
-            })
-        }
-
-        if (body.type === 'ice') {
-            wss.clients.forEach((client) => {
-                if (client !== ws && client.readyState === WS.WebSocket.OPEN) {
-                    client.send(JSON.stringify({ type: 'ice', ice: body.ice }));
-                }
-            })
-        }
-    });
+const io = new Server(PORT, {
+    cors: "*"
 });
+
+io.on('connection', (socket) => {
+    socket.on(ACTIONS.JOIN, userId => {
+        socket.broadcast.emit(ACTIONS.NEW_CLIENT, userId);
+    })
+
+    socket.on(ACTIONS.OFFER, async({ idFrom, idTo, offer }) => {
+        // Кто то отправил оффер - нам нужно его отправить
+        // console.log('new offer', idFrom, idTo, offer);
+
+        const client = socket.nsp.sockets.get(idTo);
+
+        if (!client) return;
+
+        client.emit(ACTIONS.OFFER, { idFrom, offer });
+    })
+
+    socket.on(ACTIONS.ANSWER, (data) => {
+        // Клиент создал ответ, его нужно вернуть обратно
+        const { idFrom, idTo, answer } = data;
+
+        const client = socket.nsp.sockets.get(idTo);
+
+        if (!client) return;
+
+        client.emit(ACTIONS.ANSWER, { idFrom, answer });
+    })
+
+    socket.on(ACTIONS.ICE, (data) => {
+        // { idFrom, idTo, ice }
+    })
+})
